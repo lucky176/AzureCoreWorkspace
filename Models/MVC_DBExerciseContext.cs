d4903e0a7baaaa921fff5c0d270896b11a21eaae
urverma152@gmail.com <urverma152@gmail.com>
2021-06-12 13:26:17 +0530

    Created DBContext

---
+++ Models/MVC_DBExerciseContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace azureDbSalary.Models
+{
+    public partial class MVC_DBExerciseContext : DbContext
+    {
+        public MVC_DBExerciseContext()
+        {
+        }
+
+        public MVC_DBExerciseContext(DbContextOptions<MVC_DBExerciseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:luckyserver.database.windows.net,1433;Database=MVC_DBExercise;Trusted_Connection=False;Initial Catalog=MVC_DBExercise;Encrypt=True;User ID=lucky176;Password=787898lV;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.PayBand)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
